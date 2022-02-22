f3e93bbaac3b9a914b9544c0b017f653c4f83d76
Auricelio <auricelio.suporte@hotmail.com>
2022-02-21 22:48:38 -0300

    Add Footer

---
+++ frontend/src/components/FootBar/index.tsx
@@ -0,0 +1,20 @@
+import './styles.css';
+
+function FootBar() {
+
+    return (
+
+        <footer>
+            <nav className="container">
+                <div className="sisgames-footer-content">
+                    <h1>Sistema de Avaliação de Games</h1>
+                    <a href="http://www.unifametro.edu.br/" target="_blank" rel="noreferrer">
+                    </a>
+                </div>
+            </nav>
+        </footer>
+
+    );
+}
+
+export default FootBar;
